4c2ecbdc227699a7005c8bac4f08ea5c3fd4e218
Ana Dejanović <ana.dejanovich@gmail.com>
2023-05-29 21:52:16 +0200

    Refactoring  code

--- src/App.jsx
+++ src/App.jsx
@@ -17,10 +17,6 @@ function App() {
   const addTask = () => {
     if (newTask) {
       let num = toDo.length + 1;
-      // let newEntry = { id: num, title: newTask, status: false };
-      // setToDo([...toDo, newEntry]);
-
-      // Refactored
       setToDo([...toDo, { id: num, title: newTask, status: false }]);
       setNewTask("");
     }
@@ -28,24 +24,11 @@ function App() {
 
   // Delete task
   const deleteTask = (id) => {
-    //  let newTasks = toDo.filter((task) => task.id !== id);
-    //  setToDo(newTasks);
-
-    // Refactored
     setToDo(toDo.filter((task) => task.id !== id));
   };
 
   // Mark task as done or completed
   const markDoneTask = (id) => {
-    // let newTask = toDo.map((task) => {
-    //   if (task.id === id) {
-    //     return { ...task, status: !task.status };
-    //   }
-    //   return task;
-    // });
-    // setToDo(newTask);
-
-    // Refactored
     setToDo(toDo.map((task) => (task.id === id ? { ...task, status: !task.status } : task)));
   };
 
@@ -56,14 +39,6 @@ function App() {
 
   // Change task for update
   const changeTask = (e) => {
-    // let newEntry = {
-    //   id: updateData.id,
-    //   title: e.target.value,
-    //   status: updateData.status ? true : false,
-    // };
-    // setUpdateData(newEntry);
-
-    // Refactored
     setUpdateData({
       ...updateData,
       title: e.target.value,
@@ -72,11 +47,6 @@ function App() {
 
   // Update task
   const updateTask = () => {
-    // let filterRecords = [...toDo].filter((task) => task.id !== updateData.id);
-    // let updatedObject = [...filterRecords, updateData];
-    // setToDo(updatedObject);
-
-    //Refacotred
     let removeOldRecord = [...toDo].filter((task) => task.id !== updateData.id);
     setToDo([...removeOldRecord, updateData]);
     setUpdateData("");
